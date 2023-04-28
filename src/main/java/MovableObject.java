e2f69d609692d73660ddeab8a24978f6e8e61db8
Inuinus <linusfrisk01@gmail.com>
2023-04-28 14:17:08 +0200

    Update MovableObject.java

--- src/main/java/MovableObject.java
+++ src/main/java/MovableObject.java
@@ -3,23 +3,26 @@
  *Player, Box(dock inte två boxes)
 */
 
-
 public class MovableObject{
 
-    public void moveUp(){
+    public void moveUp() {
         
     }
 
-    public void moveDown(){
-        
-    }
-    
-    public void moveLeft(){
-        
+    public void moveDown() {
+
     }
 
-    public void moveRight(){
+    public void moveLeft() {
 
+    }
+
+    public void moveRight() {
+
+    }
+
+    public void blockAhead(){
+        if()
     }
 
 }
